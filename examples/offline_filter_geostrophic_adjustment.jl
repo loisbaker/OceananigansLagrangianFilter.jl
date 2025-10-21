38158b172709d4396e06118f1cb29c5519b52743
Lois Baker <lbaker@ptolemy.maths.ed.ac.uk>
2025-10-21 17:12:54 +0100

    removed examples for now

--- examples/offline_filter_geostrophic_adjustment.jl
+++ examples/offline_filter_geostrophic_adjustment.jl
@@ -19,10 +19,8 @@ using NCDatasets
 using Printf
 
 # ### Model parameters
-Nx = 200
-Nz = 120
-# Nx = 10
-# Nz = 10
+Nx = 400
+Nz = 80
 f = 1e-4                # Coriolis frequency [s⁻¹]
 L_front = 10kilometers  # Initial front width [m]
 aspect_ratio = 100      # L_front/H
