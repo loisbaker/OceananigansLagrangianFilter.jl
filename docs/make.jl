1917f189f2b2278c2c55992e976010c672cba971
Lois Baker <lbaker@ptolemy.maths.ed.ac.uk>
2025-11-10 11:11:10 +0000

    build docs with examples

--- docs/make.jl
+++ docs/make.jl
@@ -26,17 +26,17 @@ example_scripts = [
 
 @info string("Executing the examples")
 
-# for n in 1:length(example_scripts)
-#     example = example_scripts[n]
-#     example_filepath = joinpath(EXAMPLES_DIR, example)
+for n in 1:length(example_scripts)
+    example = example_scripts[n]
+    example_filepath = joinpath(EXAMPLES_DIR, example)
     
-#     start_time = time_ns()
-#     Literate.markdown(example_filepath, OUTPUT_DIR;
-#                         flavor = Literate.DocumenterFlavor(), execute = true)
-#     elapsed = 1e-9 * (time_ns() - start_time)
-#     @info @sprintf("%s example took %s to build.", example, prettytime(elapsed))
+    start_time = time_ns()
+    Literate.markdown(example_filepath, OUTPUT_DIR;
+                        flavor = Literate.DocumenterFlavor(), execute = true)
+    elapsed = 1e-9 * (time_ns() - start_time)
+    @info @sprintf("%s example took %s to build.", example, prettytime(elapsed))
     
-# end
+end
 
 
 # Set up math rendering 
